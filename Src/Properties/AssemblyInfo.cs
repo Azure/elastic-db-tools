8ae7c3aac0836cb6ed70a92fcf8f5aa25fc53d08
Jared Moore <jaredmoo@microsoft.com>
2015-06-16 13:24:24 -0700

    Fixes fakes assembly key build error

--- Src/Properties/AssemblyInfo.cs
+++ Src/Properties/AssemblyInfo.cs
@@ -22,7 +22,6 @@ using Microsoft.Azure.SqlDatabase.ElasticScale;
 [assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.ServiceCommon" + AssemblyRef.ProductPublicKey)]
 [assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.Query.UnitTests" + AssemblyRef.ProductPublicKey)]
 [assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.ShardManagement.UnitTests" + AssemblyRef.ProductPublicKey)]
-[assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.Client.Fakes, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e92decb949446f688ab9f6973436c535bf50acd1fd580495aae3f875aa4e4f663ca77908c63b7f0996977cb98fcfdb35e05aa2c842002703cad835473caac5ef14107e3a7fae01120a96558785f48319f66daabc862872b2c53f5ac11fa335c0165e202b4c011334c7bc8f4c4e570cf255190f4e3e2cbc9137ca57cb687947bc")]
 [assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.Jobs.Common" + AssemblyRef.ProductPublicKey)]
 [assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.Jobs.Common.Tests" + AssemblyRef.ProductPublicKey)]
 [assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.Jobs.WorkerRole" + AssemblyRef.ProductPublicKey)]
@@ -43,6 +42,13 @@ using Microsoft.Azure.SqlDatabase.ElasticScale;
 [assembly: InternalsVisibleTo("SplitMergeWorker, PublicKey=00")]
 [assembly: InternalsVisibleTo("ShardSplitMergeTests, PublicKey=00")]
 #pragma warning restore 1700
+#endif
+
+// Signing is not done in external build, so this InternalsVisibleTo is different built externally
+#if EXTERNAL_BUILD
+[assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.Client.Fakes, PublicKey=00")]
+#else
+[assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.Client.Fakes, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e92decb949446f688ab9f6973436c535bf50acd1fd580495aae3f875aa4e4f663ca77908c63b7f0996977cb98fcfdb35e05aa2c842002703cad835473caac5ef14107e3a7fae01120a96558785f48319f66daabc862872b2c53f5ac11fa335c0165e202b4c011334c7bc8f4c4e570cf255190f4e3e2cbc9137ca57cb687947bc")]
 #endif
 
 // Added reference to the nuget package in the unsigned versions
