6ae687607230e2fce3917a0f4d269e4ddf3b70e0
Jared Moore <jaredmoo@microsoft.com>
2015-06-16 14:09:56 -0700

    Fixed fakes InternalsVisibleTo and assembly reference

--- Src/Properties/AssemblyInfo.cs
+++ Src/Properties/AssemblyInfo.cs
@@ -46,7 +46,7 @@ using Microsoft.Azure.SqlDatabase.ElasticScale;
 
 // Signing is not done in external build, so this InternalsVisibleTo is different built externally
 #if EXTERNAL_BUILD
-[assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.Client.Fakes, PublicKey=00")]
+[assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.Client.Fakes")]
 #else
 [assembly: InternalsVisibleTo("Microsoft.Azure.SqlDatabase.ElasticScale.Client.Fakes, PublicKey=0024000004800000940000000602000000240000525341310004000001000100e92decb949446f688ab9f6973436c535bf50acd1fd580495aae3f875aa4e4f663ca77908c63b7f0996977cb98fcfdb35e05aa2c842002703cad835473caac5ef14107e3a7fae01120a96558785f48319f66daabc862872b2c53f5ac11fa335c0165e202b4c011334c7bc8f4c4e570cf255190f4e3e2cbc9137ca57cb687947bc")]
 #endif
