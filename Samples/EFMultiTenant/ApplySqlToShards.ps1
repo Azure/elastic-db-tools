a1455ecd64b3497b78c8a2b4e3d17253fd4d828f
Jared Moore <jaredmoo@microsoft.com>
2015-08-26 13:37:53 -0700

    Migrate EFMultiTenant sample to github
    Migrate EFMultiTenant sample to github
    
    [CodeFlowLink]
    Id = jaredmoo-240999b3d2ea4a0bb57d7428a49de2d5
    Url = http://codeflow/Client/CodeFlow2010.application?review=jaredmoo-240999b3d2ea4a0bb57d7428a49de2d5&server=http://codeflow/Services/DiscoveryService.svc
    Title = Migrate EFMultiTenant sample to github
    
    Review Status: Signed Off
    
    Reviewers:
    sdoomra (required): SignedOff
    
    [tfs-changeset: 792321]

---
+++ Samples/EFMultiTenant/ApplySqlToShards.ps1
@@ -0,0 +1,21 @@
+﻿# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+# Params you must supply
+$server = ""
+$shardmapmgr = ""
+$username = ""
+$password = ""
+$sqlfile = "EnableRLS.sql"
+
+# Get shards and execute sqlfile
+$sqldir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sqlpath = Join-Path -Path $sqldir -ChildPath $sqlfile
+$query = "SELECT ServerName, DatabaseName FROM __ShardManagement.ShardsGlobal;"
+$shards = Invoke-Sqlcmd -Query $query -ServerInstance $server -Database $shardmapmgr -Username $username -Password $password
+
+foreach ($shard in $shards) {
+    # Assume all shards have same username/password as shard map manager
+    "Executing... " + $shard["DatabaseName"]
+    Invoke-Sqlcmd -InputFile $sqlpath -ServerInstance $shard["ServerName"] -Database $shard["DatabaseName"] -Username $username -Password $password
+}
---
+++ Samples/EFMultiTenant/DataClasses.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFMultiTenantElasticScale
+{
+    // Let's use the standard blogging classes from the EF tutorial
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+
+        // All tables must have a TenantId to indicate which tenant owns each row.
+        // Note: you should REMOVE TenantId from the EF data model (but not the underlying database tables) 
+        // if you plan to use default constraints, otherwise EF will automatically supply a default value
+        public int TenantId { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+
+        public virtual Blog Blog { get; set; }
+
+        public int TenantId { get; set; } // same as TenantId in Blogs
+    }
+
+    public class User
+    {
+        [Key]
+        public string UserName { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
