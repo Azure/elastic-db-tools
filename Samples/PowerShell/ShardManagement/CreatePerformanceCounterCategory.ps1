2ac8c94400719795be2f03b09f215eff068abbd5
Apurva Sahasrabudhe <apurvs@microsoft.com>
2016-05-06 16:57:54 -0700

    importing nuget packages instead of looking for dll
    importing nuget package instead of looking for a dll in current
    location.

--- Samples/PowerShell/ShardManagement/CreatePerformanceCounterCategory.ps1
+++ Samples/PowerShell/ShardManagement/CreatePerformanceCounterCategory.ps1
@@ -23,21 +23,20 @@ param(
 	[string]
 	$UserName=""
 )
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Path
-$dllNameWithPath = $scriptPath + "\Microsoft.Azure.SqlDatabase.ElasticScale.Client.dll";
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+# Import modules
+$ScriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+Import-Module $ScriptDir\ShardManagement -Force
 
 if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    if (Test-Path $dllNameWithPath) {
-        [system.reflection.assembly]::LoadFile($dllNameWithPath)
-        Write-Host "Creating performance counter category 'Elastic Database: Shard Management' ..."
-        [Microsoft.Azure.SqlDatabase.ElasticScale.ShardManagement.ShardMapManagerFactory]::CreatePerformanceCategoryAndCounters()
-        Write-Host "Performance counter category 'Elastic Database: Shard Management' created successfully."
-        Write-Host "Adding specified user to 'Performance Monitor Users' group ..."
-        net localgroup "Performance Monitor Users" $UserName /ADD
-        Write-Host "User" $UserName "is now part of 'Performance Monitor Users' group"
-    } else {
-            Write-Host "Please copy Microsoft.Azure.SqlDatabase.ElasticScale.Client.dll in this folder and rerun the script."
-    }
+    Write-Host "Creating performance counter category 'Elastic Database: Shard Management' ..."
+    [Microsoft.Azure.SqlDatabase.ElasticScale.ShardManagement.ShardMapManagerFactory]::CreatePerformanceCategoryAndCounters()
+    Write-Host "Performance counter category 'Elastic Database: Shard Management' created successfully."
+    Write-Host "Adding specified user to 'Performance Monitor Users' group ..."
+    net localgroup "Performance Monitor Users" $UserName /ADD
+    Write-Host "User" $UserName "is now part of 'Performance Monitor Users' group"
 } else {
     Write-Host "Performance counter catagory creation needs Administrator privileges, please rerun script as Administrator."
 }
