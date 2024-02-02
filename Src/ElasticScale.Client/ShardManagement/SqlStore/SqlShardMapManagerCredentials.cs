563f9d9631c40d3d5e87930fb69c5a59f8ab8c4c
Bill Pieper <Bill.Pieper@outlook.com>
2024-02-02 08:37:47 -0600

    updated

--- Src/ElasticScale.Client/ShardManagement/SqlStore/SqlShardMapManagerCredentials.cs
+++ Src/ElasticScale.Client/ShardManagement/SqlStore/SqlShardMapManagerCredentials.cs
@@ -161,7 +161,7 @@ namespace Microsoft.Azure.SqlDatabase.ElasticScale.ShardManagement
             if (connectionString.ContainsKey(ShardMapUtils.Authentication))
             {
                 string authentication = connectionString[ShardMapUtils.Authentication].ToString();
-                if ( authentication.Equals(ShardMapUtils.ActiveDirectoryDefaultStr, StringComparison.OrdinalIgnoreCase)
+                if (authentication.Equals(ShardMapUtils.ActiveDirectoryDefaultStr, StringComparison.OrdinalIgnoreCase)
                     || authentication.Equals(ShardMapUtils.ActiveDirectoryIntegratedStr, StringComparison.OrdinalIgnoreCase)
                     || authentication.Equals(ShardMapUtils.ActiveDirectoryInteractiveStr, StringComparison.OrdinalIgnoreCase)
                     || authentication.Equals(ShardMapUtils.ActiveDirectoryManagedIdentity, StringComparison.OrdinalIgnoreCase)
